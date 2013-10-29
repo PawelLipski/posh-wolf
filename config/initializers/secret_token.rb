2c9fd1e3508c8260fb84bd590d44a9d50983eba4
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-10-29 21:12:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PoshWolf::Application.config.secret_key_base = '3bd2d0b445426ef6db9110fba168d4813f3061a77bbe13362b2456d077400fb0d8a2e2648266ac8a6657ebce77abbdd0a547729133414871e6488f11bc73c52e'
