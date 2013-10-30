e7a69c5baa7f41fd395b36cfaaf617f00429f8e2
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-10-30 10:13:42 +0100

    List mongo collections

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,11 @@
+include Mongo
 
 class MainController < ApplicationController
   def index
     @text = "Hello"
+    mongo_client = MongoClient.from_uri("mongodb://hgt:hgt123@ds041178.mongolab.com:41178/hgtdb")
+    db = mongo_client.db("hgtdb")
+    @coll_names = db.collection_names
+    #.each { |name| puts name }
   end
 end
