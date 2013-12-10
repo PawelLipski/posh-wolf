03ef3c91657ad9b744224a657318982e13982869
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-12-10 20:24:46 +0100

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,78 +1,17 @@
+
 PoshWolf::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
   root "main#index"   
 
-  post "upload" => "main#upload"
-  
-  get "testcase/async" => "main#get_async"
+  post "ajax/post-task-from-pasted" => "ajax#post_task_from_pasted"
+  post "ajax/post-task-from-url" => "ajax#post_task_from_url"
+  post "ajax/post-task-from-sample" => "ajax#post_task_from_sample"
 
-  post "testcase/ajax/post-task" => "main#ajax_post_task"
-  
-  post "testcase/ajax/post-task-from-pasted" => "main#ajax_post_task_from_pasted"
-  
-  post "testcase/ajax/post-task-from-url" => "main#ajax_post_task_from_url"
-  
-  post "testcase/ajax/post-task-from-sample" => "main#ajax_post_task_from_sample"
+  get "ajax/get-all-progresses" => "ajax#get_all_progresses"
+  get "ajax/get-result" => "ajax#get_result"
 
-  get "testcase/ajax/get-all-progresses" => "main#ajax_get_all_progresses"
-
-  get "testcase/ajax/get-result" => "main#ajax_get_result"
-
-  get "ajax/load-animation" => "main#ajax_load_animation"
+  get "ajax/load-animation" => "ajax#load_animation"
 
   get "sleep" => "main#do_sleep"
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-  
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
