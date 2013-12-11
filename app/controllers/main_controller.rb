75d23edddcc24ed00011445f0e3d02f71934029c
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-12-11 15:07:10 +0100

    Add problem description

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,6 +4,12 @@ class MainController < ApplicationController
   def index
     @groups = [[20,5], [20, 10], [20, 20], [50, 5], [50, 10], [50, 20]]
   end
+
+  def about
+  end
+
+  def flow_shop
+  end
   
   def do_sleep
     sleep 10
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@
 PoshWolf::Application.routes.draw do
 
   root "main#index"   
+  get  "about" => "main#about"
+  get  "flow-shop" => "main#flow_shop"
 
   post "ajax/post-task-from-pasted" => "ajax#post_task_from_pasted"
   post "ajax/post-task-from-url" => "ajax#post_task_from_url"
   post "ajax/post-task-from-sample" => "ajax#post_task_from_sample"
 
-  get "ajax/get-all-progresses" => "ajax#get_all_progresses"
-  get "ajax/get-result" => "ajax#get_result"
+  get  "ajax/get-all-progresses" => "ajax#get_all_progresses"
+  get  "ajax/get-result" => "ajax#get_result"
 
-  get "ajax/load-animation" => "ajax#load_animation"
+  get  "ajax/load-animation" => "ajax#load_animation"
 
-  get "sleep" => "main#do_sleep"
+  get  "sleep" => "main#do_sleep"
 
 end
