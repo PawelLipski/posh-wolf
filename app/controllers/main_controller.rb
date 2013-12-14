b5f16d645559e386ba6470d7a7e04bc309ed9549
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-12-14 10:55:22 +0100

    Add test for SingleThreadWorker

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,18 @@
 
 class MainController < ApplicationController
+  
+  include LoggerHelper
 
   def index
     @groups = [[20,5], [20, 10], [20, 20], [50, 5], [50, 10], [50, 20]]
+    
+    worker = SingleThreadWorker.new
+    for i in 1..5
+      worker.post do
+        sleep 10
+        puts "foo"
+      end
+    end
   end
 
   def problem
