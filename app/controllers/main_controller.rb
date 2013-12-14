c7f5fb584db49dcfb2c7bad97e0cc18b763fec46
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-12-14 12:26:19 +0100

    Access to Mongo with credentials stored in ENV

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,14 +5,8 @@ class MainController < ApplicationController
 
   def index
     @groups = [[20,5], [20, 10], [20, 20], [50, 5], [50, 10], [50, 20]]
-    
-    worker = SingleThreadWorker.new
-    for i in 1..5
-      worker.post do
-        sleep 10
-        puts "foo"
-      end
-    end
+  
+    Logger.new
   end
 
   def problem
--- app/helpers/logger_helper.rb
+++ app/helpers/logger_helper.rb
@@ -24,5 +24,25 @@ module LoggerHelper
 
   end
 
+  class Logger
+
+    include Mongo
+
+    def initialize
+      user = ENV['POSH_WOLF_MONGO_USER']
+      passwd = ENV['POSH_WOLF_MONGO_PASSWD']
+      puts "#{user} #{passwd}"
+      if user and passwd
+        uri = "mongodb://#{user}:#{passwd}@ds061158.mongolab.com:61158/posh-wolf-logs"
+        @mongo_client = MongoClient.from_uri(uri)
+        @db = @mongo_client.db("posh-wolf-logs")
+        @coll = @db["webapp-logs"]
+
+        puts @coll
+      end
+    end
+
+  end
+
 end
 
