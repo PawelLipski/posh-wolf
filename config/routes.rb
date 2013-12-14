5c9544f256868021d23a452915f7451982ce5b15
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-12-14 14:02:36 +0100

    Add redirect to Taillard page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ PoshWolf::Application.routes.draw do
   get  "about" => "main#about"
   get  "contact" => "main#contact"
 
+  get "taillard" => redirect("http://mistic.heig-vd.ch/taillard/problemes.dir/ordonnancement.dir/ordonnancement.html")
+
   post "ajax/post-task-from-pasted" => "ajax#post_task_from_pasted"
   post "ajax/post-task-from-url" => "ajax#post_task_from_url"
   post "ajax/post-task-from-sample" => "ajax#post_task_from_sample"
