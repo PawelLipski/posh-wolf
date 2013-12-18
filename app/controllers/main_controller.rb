996a6f764c705ac63d43a00682efee7d7b3063ef
Pawel Lipski <pawel.p.lipski@gmail.com>
2013-12-18 10:04:51 +0100

    Remove any Mongo-related code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,8 @@
 
 class MainController < ApplicationController
   
-  include LoggerHelper
-
   def index
     @groups = [[20,5], [20, 10], [20, 20], [50, 5], [50, 10], [50, 20]]
-  
-    Logger.new
   end
 
   def problem
