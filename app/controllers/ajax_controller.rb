ca5e2dc724ff3bee66884d141544706a7b396a67
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-01-08 13:06:39 +0100

    Go back to local WS

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -99,8 +99,8 @@ class AjaxController < ApplicationController
     end
     
     def execute_soap_request(method, args, retval)
-      #ep = "http://0.0.0.0:8080"
-      ep = "http://posh-wolf-ws.herokuapp.com" 
+      ep = "http://0.0.0.0:8080"
+      #ep = "http://posh-wolf-ws.herokuapp.com" 
       
       respond_to do |format|
         format.js do 
