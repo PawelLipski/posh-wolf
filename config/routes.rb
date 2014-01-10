458537c3aed103bc18d81db344cb16f10f62a510
Pawel Lipski <pawel.p.lipski@gmail.com>
2014-01-10 19:01:00 +0100

    Add redirect to favicon

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ PoshWolf::Application.routes.draw do
   get  "about" => "main#about"
   get  "contact" => "main#contact"
 
-  get "collection" => redirect("http://mistic.heig-vd.ch/taillard/problemes.dir/ordonnancement.dir/ordonnancement.html")
+  get  "collection" => redirect("http://mistic.heig-vd.ch/taillard/problemes.dir/ordonnancement.dir/ordonnancement.html")
 
-  get "webapp-src" => redirect('http://github.com/tilius/posh-wolf')
-  get "webtests-src" => redirect('http://bitbucket.org/bartexki/poshwolftest')
-  get "ws-src" => redirect('http://github.com/tilius/posh-wolf-ws')
+  get  "webapp-src" => redirect('http://github.com/tilius/posh-wolf')
+  get  "webtests-src" => redirect('http://bitbucket.org/bartexki/poshwolftest')
+  get  "ws-src" => redirect('http://github.com/tilius/posh-wolf-ws')
 
   post "ajax/post-task-from-pasted" => "ajax#post_task_from_pasted"
   post "ajax/post-task-from-url" => "ajax#post_task_from_url"
@@ -21,6 +21,8 @@ PoshWolf::Application.routes.draw do
   get  "ajax/get-all-progresses" => "ajax#get_all_progresses"
   get  "ajax/get-result" => "ajax#get_result"
   get  "ajax/load-animation" => "ajax#load_animation"
+
+  get  "favicon.ico" => redirect("/assets/favicon.ico")
 
   get '*path' => redirect('/') unless Rails.env.development?
 end
